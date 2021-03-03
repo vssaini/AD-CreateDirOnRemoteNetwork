cc9359dc6bbcb9c0ec83db40744ceb6d93d373f2
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 06:50:22 +0530

    Add project files.

---
+++ CreateDirOnRemoteNetwork/Helpers/Utility.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Management;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace CreateDirOnRemoteNetwork.Helpers
+{
+    /// <summary>
+    /// Provide helper functions.
+    /// </summary>
+    class Utility
+    {
+        [DllImport("shlwapi.dll")]
+        public static extern bool PathIsNetworkPath(string pszPath);
+
+        /// <summary>
+        /// Create directory in Shared folder over UNC path.
+        /// </summary>
+        /// <param name="networkName">The network name as \\192.168.1.202\Data\</param>
+        /// <param name="directoryFullName">The network directory as \\192.168.1.202\Data\VikramDirectory</param>       
+        public static void CreateDirectoryInShare(string networkName, string directoryFullName)
+        {
+            if (PathIsNetworkPath(directoryFullName))
+            {
+                var nw = GetNetworkCredential();
+                using (new NetworkConnection(networkName, nw))
+                {
+                    // Create directory
+                    var dirInfo = new DirectoryInfo(directoryFullName);
+                    
+                    var dirExists = dirInfo.Exists;
+                    if (!dirExists)
+                        dirInfo.Create();
+                }
+            }
+            else
+            {
+                // Create locally
+                var dirInfo = new DirectoryInfo(directoryFullName);
+
+                var dirExists = dirInfo.Exists;
+                if (!dirExists)
+                    dirInfo.Create();
+            }
+        }
+
+        /// <summary>
+        /// Read shared file from UNC path.
+        /// </summary>
+        /// <param name="networkName">The network name as \\192.168.1.202\data</param>
+        /// <param name="networkFile">The network file as \\192.168.1.202\data\AboutMe.txt</param>
+        /// <returns>Return value from file</returns>
+        public static string ReadShareFile(string networkName, string networkFile)
+        {
+            string fileText;
+            if (PathIsNetworkPath(networkFile))
+            {
+                var nw = GetNetworkCredential();
+                using (new NetworkConnection(networkName, nw))
+                {
+                    // Read file
+                    if (File.Exists(networkFile))
+                    {
+                        fileText = File.ReadAllText(networkFile);
+                    }
+                    else
+                    {
+                        throw new Exception(string.Format("File '{0}' not found.", networkFile));
+                    }
+                }
+            }
+            else
+            {
+                // Read file
+                if (File.Exists(networkFile))
+                {
+                    fileText = File.ReadAllText(networkFile);
+                }
+                else
+                {
+                    throw new Exception(string.Format("File '{0}' not found.", networkFile));
+                }
+            }
+
+            return fileText;
+        }
+
+        /// <summary>
+        /// Get UNC path from the path passed
+        /// </summary>
+        /// <param name="filePath">The file path that need to be evaluated</param>
+        /// <returns>Return UNC path</returns>
+        public static string GetUNCPath(string filePath)
+        {
+            var path = filePath.TrimEnd('\\', '/') + Path.DirectorySeparatorChar;
+            var dirInfo = new DirectoryInfo(path);
+            var root = dirInfo.Root.FullName.TrimEnd('\\');
+
+            if (!root.StartsWith(@"\\"))
+            {
+                var mo = new ManagementObject
+                {
+                    Path = new ManagementPath(string.Format("Win32_LogicalDisk='{0}'", root))
+                };
+
+                // Get drive type and set path as per that
+                var driveType = Convert.ToUInt32(mo["DriveType"]);
+
+                switch (driveType)
+                {
+                    case 0: // Unknown
+                    case 1: // No root directory
+                    case 2: // Removable disk
+                    case 5: // Compact Disk
+                    case 6: // RAM Disk
+                        break;
+
+                    case 3: // Local disk
+                         // root = string.Format(@"\\{0}\{1}$\", Dns.GetHostName(), root.TrimEnd(':'));
+                        break;
+
+                    case 4: // Network Drive
+                        root = Convert.ToString(mo["ProviderName"]);
+                        filePath = Recombine(root, dirInfo);
+                        break;
+                }
+            }
+
+            return filePath;
+        }
+
+        #region Helpers
+
+        private static string Recombine(string root, DirectoryInfo dirInfo)
+        {
+            var stack = new Stack();
+            while (dirInfo.Parent != null)
+            {
+                stack.Push(dirInfo.Name);
+                dirInfo = dirInfo.Parent;
+            }
+
+            while (stack.Count > 0)
+            {
+                root = Path.Combine(root, (string)stack.Pop());
+            }
+            return root;
+        }
+
+        /// <summary>
+        /// Get network credential object.
+        /// </summary>
+        private static NetworkCredential GetNetworkCredential()
+        {
+            var nw = new NetworkCredential
+            {
+                Domain = "domain",
+                UserName = "Administrator",
+                Password = "Pass99"
+            };
+
+            return nw;
+        }
+
+        #endregion
+
+    }
+}
---
+++ CreateDirOnRemoteNetwork/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CreateDirOnRemoteNetwork.Helpers;
+
+namespace CreateDirOnRemoteNetwork
+{
+    internal class Program
+    {
+		// Ref - https://technet.microsoft.com/en-us/library/ee791865(v=ws.10).aspx
+
+		// 'data' is the share name and also folder on resepctive system
+		// 'Server name' instead of IP Address will not work		
+        private const string NetworkName = @"\\192.168.1.202\Data";
+        private const string NetworkDirFullName = @"\\192.168.1.202\Data\VikramDirectory";
+
+        private static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Creating directory over network share or mapped drive...\n\n");
+                CreateDirectoryAtUncPath();
+
+                //GetUncPath();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ResetColor();
+            }
+
+            // Hold till we read
+            Console.ReadLine();
+        }
+
+        private static void CreateDirectoryAtUncPath()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Utility.CreateDirectoryInShare(NetworkName, NetworkDirFullName);
+            Console.WriteLine("Directory created at path '{0}'", NetworkDirFullName);
+            Console.ResetColor();
+        }
+
+        private static void GetUncPath()
+        {
+            //const string path = @"Z:\25UsersImport.csv"; // By default drive letter is Z
+            const string path = @"E:\Xtra";
+
+            if (path.StartsWith(@"\") && Utility.PathIsNetworkPath(path))
+            {
+                //ReadFromUncPath();
+                Console.ForegroundColor = ConsoleColor.Green;
+                var uncPath = Utility.GetUNCPath(path);
+                Console.WriteLine("The UNC path returned for path '{0}' -\n\n{1}", path, uncPath);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                var uncPath = Utility.GetUNCPath(path);
+                Console.WriteLine("The local path returned for path '{0}' -\n\n{1}", path, uncPath);
+                Console.ResetColor();
+            }
+
+        }
+    }
+}
